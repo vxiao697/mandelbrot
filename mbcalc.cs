d2ce1387e49908af4daad931911ab07117e232d7
vvv317 <vincentxiao697@gmail.com>
2021-09-28 22:00:53 -0700

    Initial commit
    setup

---
+++ mbcalc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mandelbrot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
